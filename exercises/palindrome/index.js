dc065a3f4bb52c36b439502f85959b95f758f407
DavidOliv1 <davidstna21@gmail.com>
2024-03-23 17:39:15 -0300

    adds solution to palindrome problem

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,38 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// solution 1:
+// function palindrome(str) {
+//   return str === str.split("").reverse().join("");
+// }
+
+// solution 2:
+// function palindrome(str) {
+//   return str
+//     .split("")
+//     .every((char, i, str) => char === str[str.length - (i + 1)]);
+// }
+
+// solution 3:
+// function palindrome(str) {
+//   const half = Math.floor(str.length / 2);
+//   for (let i = 0; i < half; i++) {
+//     if (str[i] !== str[str.length - (i + 1)]) return false;
+//   }
+//   return true;
+// }
+
+// solution 4:
+function palindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
 
 module.exports = palindrome;
