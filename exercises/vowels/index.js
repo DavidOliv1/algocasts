3eb85155f3fadcd715f515aa9f947c1fafabb0f0
DavidOliv1 <davidstna21@gmail.com>
2024-03-31 21:40:51 -0300

    adds solution to vowels problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,36 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// solution 1:
+// function vowels(str) {
+//   const regexp = /[AEIOU]/gi;
+//   const result = str.match(regexp);
+//   return result?.length || 0;
+// }
+
+// solution 2:
+// function vowels(str) {
+//   return str.replace(/[^aeiou]/gi, "").length;
+// }
+
+// solution 3:
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ["a", "e", "i", "o", "u"];
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// solution 4:
+function vowels(str) {
+  return str.split("").reduce((acc, char) => {
+    acc += ["a", "e", "i", "o", "u"].includes(char.toLowerCase()) ? 1 : 0;
+    return acc;
+  }, 0);
+}
 
 module.exports = vowels;
